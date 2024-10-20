b10904015ac31210c880698159ab1c2b119f623f
Neeraj-7-sudo <51938433+Neeraj-7-sudo@users.noreply.github.com>
2024-10-20 23:58:22 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,6 +3,6 @@
 const { test, expect } = require('@playwright/test');
 
 test('Test Qase - Playwright Integration', async ({ page }) => {
-  await page.goto('https://app.qase.io/');
+  await page.goto('https://qase.io/');
   expect(await page.title()).toBe('Qase | Test management software for quality assurance');
 });
